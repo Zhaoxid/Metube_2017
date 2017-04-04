79639d0ef0d8e4fa616be0c9e61aae619ac69af4
Zhaoxid <zhaoxid@clemson.edu>
2017-04-04 09:44:16 -0400

    Test

--- browse.php
+++ browse.php
@@ -6,7 +6,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Media browse</title>
+<title>Media Browse</title>
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <script type="text/javascript" src="js/jquery-latest.pack.js"></script>
 <script type="text/javascript">
