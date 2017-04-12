18601dfbb8cfc9332ba4c045a1ce309fc5b872ff
Zhaoxid <zhaoxid@clemson.edu>
2017-04-12 15:35:27 -0400

    sql

--- create_table.sql
+++ create_table.sql
@@ -3,6 +3,6 @@ create table account (username varchar(30) NOT NULL PRIMARY KEY, password varcha
 create table media (filename varchar(40), username varchar(40), type varchar(30), mediaid int NOT NULL AUTO_INCREMENT PRIMARY KEY, path varchar(100));
 
 create table message (message varchar (3000), subj varchar(500), sdusername varchar(40), rcvusername varchar(40),
-msgid int NOT NULL AUTO_INCREMENT PRIMARY KEY, ts timestamp NOT NULL);
+msgid int NOT NULL AUTO_INCREMENT PRIMARY KEY, ts timestamp NOT NULL CURRENT_TIMESTAMP );
 
-
+create table contacts (username1 varchar(30), relation varchar (10), username2 varchar(30));
