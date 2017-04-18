5d5583a8693ef102731ac4609a2357bfdce694cd
Matthew Beazley <mbeazle@g.clemson.edu>
2017-04-17 22:10:38 -0400

    duplication fix

--- media.php
+++ media.php
@@ -44,19 +44,6 @@ if(isset($_GET['id'])) {
 ?>
 	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
 	<h2>Viewing Video:<?php echo $result_row[6];?></h2>
-	      
-    <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
-
-<param name="filename" value="<?php echo $result_row[4];?>">
-	<!-- echo $result_row[2].$result_row[1];  -->
-		
-
-<param name="Showcontrols" value="True">
-<param name="autoStart" value="True">
-
-<embed type="application/x-mplayer2" src="<?php echo $filepath;  ?>" name="MediaPlayer" width=320 height=240></embed>
-
-</object>
 
 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
 codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="550"
