81fdc08b77e32d485c03cd7a7bc63801df58c78e
Stuart L Jackson <sljacks@webapp.cs.clemson.edu>
2017-04-19 17:33:22 -0400

    Adds formatting to the web pages.

--- contacts.php
+++ contacts.php
@@ -1,4 +1,11 @@
 <!DOCTYPE HTML>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/default.css" />
+
 <?php
     session_start();
     include_once "function.php";
@@ -25,6 +32,35 @@ if(isset($_POST['submit'])) {
 
 <html>
 <head>
+<!-- Sidebar -->
+<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" style="z-index:3;width:250px;margin-top:43px;" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
+    <i class="fa fa-remove"></i>
+  </a>
+  <h4 class="w3-bar-item"><b>Menu</b></h4>
+  <a class="w3-bar-item w3-button w3-hover-black" href='profile.php'>Profile</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='browse.php'>Browse</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='media_upload.php'>Upload File</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='playlist.php'>Playlist</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='message.php'>Message&Inbox</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='contacts.php'>Contacts</a><br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='index.php'>Logout</a> <br>
+
+</nav>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style="margin-left:250px">
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Contacts</h1>
+
+
+
     <title> Contacts </title>
     <style>
         td, th {
@@ -33,8 +69,6 @@ if(isset($_POST['submit'])) {
         }
     </style>
 </head>
-<h1> <?php echo "Welcome,", $_SESSION['username'],"!" ?></h1>
-<a href='browse.php'  style="color:#FF9900;">Return</a> <br><br>
 <body>
 <form action="contacts.php" method="post" >
     Add Contacts: <input type="username2" name = "user2" required></input><br/>
@@ -52,8 +86,7 @@ if (!$result){
     die ("Could not query the media table in the database: <br />". mysql_error());
 }
 ?>
-<div style="background:#339900;color:#FFFFFF; width:200px;">Contacts List</div>
-<table>
+<!-- <div style="background:#339900;color:#FFFFFF; margin-left:250px; width:200px;">Contacts List</div> -->
     <col width="30">
     <col width="100">
     <col width="100">
--- playlist.php
+++ playlist.php
@@ -5,13 +5,48 @@ include_once "function.php";
 $request = 0;
 ?>
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/default.css" />
+
 <head>
-    <title> Playlist</title>
+
+<!-- Sidebar -->
+<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" style="z-index:3;width:250px;margin-top:43px;" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
+    <i class="fa fa-remove"></i>
+  </a>
+  <h4 class="w3-bar-item"><b>Menu</b></h4>
+  <a class="w3-bar-item w3-button w3-hover-black" href='profile.php'>Profile</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='browse.php'>Browse</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='media_upload.php'>Upload File</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='playlist.php'>Playlist</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='message.php'>Message&Inbox</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='contacts.php'>Contacts</a><br>
+  <a class="w3-bar-item w3-button w3-hover-black" href='index.php'>Logout</a> <br>
+
+</nav>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style="margin-left:250px">
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Playlists</h1>
+        <form action = "search.php" method="GET" >
+        <input type = 'text' size='90' name='search' >
+        <input type = 'submit' name='submit' value='Search files'>
+        </form>
+
 </head>
 <body>
-<p class="bold">Welcome to metube, <?php echo $_SESSION['username'];?>!</p></br></br>
 
-<a href='browse.php'  style="color:#FF9900;">Return</a> <br><br>
 <form action="create_playlist.php" method="post" ><input type="text" name="playlistname" placeholder="Enter Playlist Name" required>
 <input value="Create Playlist" name="submit_playlist" type="submit" /></form>
 
