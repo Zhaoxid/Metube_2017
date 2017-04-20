1fd2246d3f91dbd98dd4996b14d9059d21f22514
Zhaoxid <zhaoxid@clemson.edu>
2017-04-20 12:10:59 -0400

    fix

--- submit_comment.php
+++ submit_comment.php
@@ -4,33 +4,33 @@ session_start();
 include_once "function.php";
 if(empty($_SESSION['username'])) {
     Print '<script>alert("User not found");</script>';
-    Print '<script>window.location.assign("index.php");</script>';
-}
+    if (isset($_GET['id'])) {
+        $mediaid = $_GET['id'];
+    }
+} else {
+    if (isset($_GET['id'])) {
+        $mediaid = $_GET['id'];
+    }
+    $username = $_SESSION['username'];
+    $cmt = $_POST['comment'];
 
+    if (!$cmt) {
+        echo "Please enter the comment!";
+        exit(-1);
+    }
 
-if(isset($_GET['id'])) {
-    $mediaid = $_GET['id'];
-}
-$username = $_SESSION['username'];
-$cmt = $_POST['comment'];
+    $insert = "insert into comments(cmt, username, mediaid, ts)" .
+        "values('$cmt','$username', '$mediaid', NULL)";
 
-if (!$cmt) {
-    echo "Please enter the comment!";
-    exit(-1);
-}
+    $insertresult = mysql_query($insert);
 
-$insert = "insert into comments(cmt, username, mediaid, ts)".
-    "values('$cmt','$username', '$mediaid', NULL)";
+    if (!$insertresult) {
+        echo "insert error";
+        exit(-1);
+    }
 
-$insertresult = mysql_query($insert);
-
-if(!$insertresult) {
-    echo "insert error";
-    exit(-1);
-}
-
-echo "<script type='text/javascript'>alert('Changed successfully!')</script>";
-
+    echo "<script type='text/javascript'>alert('Changed successfully!')</script>";
 ?>
 <meta http-equiv="refresh" content="0; url=media.php?id=<?php echo $mediaid;?>" />
+<?php } ?>
 </html>
