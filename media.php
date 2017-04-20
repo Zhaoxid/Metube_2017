ee3996995b84e6ee9a5fb286ade08dded11bc4fb
Stuart L Jackson <sljacks@webapp.cs.clemson.edu>
2017-04-20 13:54:43 -0400

    Removed media sidebar

--- media.php
+++ media.php
@@ -26,25 +26,6 @@
 
 <body>
 
-<!-- Sidebar -->
-<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" style="z-index:3;width:250px;margin-top:43px;" id="mySidebar">
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
-    <i class="fa fa-remove"></i>
-  </a>
-  <h4 class="w3-bar-item"><b>Menu</b></h4>
-  <a class="w3-bar-item w3-button w3-hover-black" href='profile.php'>Profile</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='browse.php'>Browse</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='media_upload.php'>Upload File</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='playlist.php'>Playlist</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='message.php'>Message&Inbox</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='contacts.php'>Contacts</a><br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='logout.php'>Logout</a> <br>
-
-</nav>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
 <!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
 <div class="w3-main" style="margin-left:250px">
 
