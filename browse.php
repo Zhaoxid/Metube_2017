bb78830c1948b783334bfb4a158eec2cef68210b
Zhaoxid <zhaoxid@clemson.edu>
2017-04-20 12:33:49 -0400

    fix

--- browse.php
+++ browse.php
@@ -52,15 +52,16 @@ function saveDownload(id)
     <i class="fa fa-remove"></i>
   </a>
   <h4 class="w3-bar-item"><b>Menu</b></h4>
-  <a class="w3-bar-item w3-button w3-hover-black" href='profile.php'>Profile</a> <br>
+
   <a class="w3-bar-item w3-button w3-hover-black" href='browse.php'>Browse</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='media_upload.php'>Upload File</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='playlist.php'>Playlist</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='message.php'>Message&Inbox</a> <br>
-  <a class="w3-bar-item w3-button w3-hover-black" href='contacts.php'>Contacts</a><br>
     <?php if(empty($_SESSION['username'])) {?>
   <a class="w3-bar-item w3-button w3-hover-black" href='index.php'>Index</a> <br>
   <?php } else { ?>
+        <a class="w3-bar-item w3-button w3-hover-black" href='profile.php'>Profile</a> <br>
+        <a class="w3-bar-item w3-button w3-hover-black" href='media_upload.php'>Upload File</a> <br>
+        <a class="w3-bar-item w3-button w3-hover-black" href='playlist.php'>Playlist</a> <br>
+        <a class="w3-bar-item w3-button w3-hover-black" href='message.php'>Message&Inbox</a> <br>
+        <a class="w3-bar-item w3-button w3-hover-black" href='contacts.php'>Contacts</a><br>
         <a class="w3-bar-item w3-button w3-hover-black" href='logout.php'>Logout</a> <br>
     <?php }?>
 </nav>
